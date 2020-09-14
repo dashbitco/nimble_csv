8ee95f06aacfee16d10ebc57e50917ae1702c339
Guilherme 'nirev' de Maio <guilherme@nirev.org>
2020-09-14 12:26:41 -0300

    typo fix in NimbleCSV.Spreadsheet moduledoc (#56)

--- lib/nimble_csv.ex
+++ lib/nimble_csv.ex
@@ -572,7 +572,7 @@ NimbleCSV.define(NimbleCSV.Spreadsheet,
   A parser with spreadsheet friendly settings.
 
   The parser uses tab as separator and double-quotes as escape, as required by
-  common spreadsheet softwtare such as Excel, Numbers and OpenOffice. It's encoded
+  common spreadsheet software such as Excel, Numbers and OpenOffice. It's encoded
   in UTF-16 little-endian with a byte-order BOM.
   """
 )
