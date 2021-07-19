bf7e020743fa41a44b9fdeb3a3f8ef694ac1300c
José Valim <jose.valim@dashbit.co>
2021-07-19 18:56:31 +0200

    Mention extension, closes #66

--- lib/nimble_csv.ex
+++ lib/nimble_csv.ex
@@ -637,5 +637,7 @@ NimbleCSV.define(NimbleCSV.Spreadsheet,
   The parser uses tab as separator and double-quotes as escape, as required by
   common spreadsheet software such as Excel, Numbers and OpenOffice. It's encoded
   in UTF-16 little-endian with a byte-order BOM.
+
+  Such files should still be saved with the `.csv` extension.
   """
 )
