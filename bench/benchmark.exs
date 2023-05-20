3ad0be9e5c77424e53d4e59e02c95620a652ec61
Vinícius Müller <vinigm.nho@gmail.com>
2023-05-19 21:55:52 -0300

    Add benchmarks using benchee

---
+++ bench/benchmark.exs
@@ -0,0 +1,22 @@
+sales_csv = "bench/data/10000_sales.csv"
+sales_string = File.read!(sales_csv)
+sales_list = String.split(sales_string, "\n")
+sales_stream = File.stream!(sales_csv)
+
+Benchee.run(
+  %{
+    "10k lines, 14 columns - NimbleCSV.RFC4180.parse_string/1" => fn -> 
+      NimbleCSV.RFC4180.parse_string(sales_string)
+    end,
+
+    "10k lines, 14 columns - NimbleCSV.RFC4180.parse_stream/1" => fn -> 
+      sales_stream
+      |> NimbleCSV.RFC4180.parse_stream()
+      |> Enum.to_list()
+    end,
+
+    "10k lines, 14 columns - NimbleCSV.RFC4180.parse_enumerable/1" => fn -> 
+      NimbleCSV.RFC4180.parse_enumerable(sales_list)
+    end
+  }
+)
